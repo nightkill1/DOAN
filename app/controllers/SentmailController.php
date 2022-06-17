4fa942ff7e40fcf8a85a18b8927d977a9f7aa887
nightkill1 <55351968+nightkill1@users.noreply.github.com>
2022-06-17 20:26:31 +0700

    Update SentmailController.php

--- app/controllers/SentmailController.php
+++ app/controllers/SentmailController.php
@@ -31,7 +31,7 @@ class SentmailController extends Controller {
             $mail->Host = 'smtp.gmail.com';                    //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = "hoanganhhai123123@gmail.com";                     //SMTP username
-            $mail->Password   = 'hoanganha123';                               //SMTP password
+            $mail->Password   = 'xxxxxxxxxxxx';                               //SMTP password
             $mail->SMTPSecure = 'tls';        //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                          //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             
